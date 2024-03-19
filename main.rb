ed957cb0a30f2beba07deec580862c9fe9998040
yagijin <yagijin_dev@ymail.ne.jp>
2024-03-19 23:21:01 +0900

    テキストをパースできない場合に無視して次の行の走査に入る

--- main.rb
+++ main.rb
@@ -3,15 +3,19 @@
 
 result = Hash.new
 sum = 0
-
-print ARGV[0]
-
+i = 0
 File.open("./data/#{ARGV[0]}"){ |f|
   f.each_line{ |line|
     rows = line.split(" ")
     rows.pop(1)
-    price = rows.pop().split(',').join('').to_i
-    sum += price
+
+    begin
+      price = rows.pop().split(',').join('').to_i
+      sum += price
+    rescue
+      print "some error occured\n"
+      next
+    end
 
     if result[rows[1]] == nil
       result[rows[1]] = price
@@ -21,12 +25,18 @@ File.open("./data/#{ARGV[0]}"){ |f|
   }
 }
 
-print "実行対象: #{ARGV[0]}\\n"
-print "合計: #{sum}円\\n"
+print "実行対象: #{ARGV[0]}\n"
+print "合計: #{sum}円\n"
 
 sorted_result = result.sort {|a,b| a[1]<=>b[1]}
 
-sorted_result.each do | key, value |
-  print "\\n #{key}: #{value} \\n"
-  print "#" * ((value / 1000) + 1)
-end
+File.open("./result/#{ARGV[0]}", "w") { |f|
+
+  f.puts "実行対象: #{ARGV[0]}"
+  f.puts "合計: #{sum}円"
+
+  sorted_result.each do | key, value |
+    f.puts "#{key}: #{value}"
+    f.puts "#" * ((value / 1000) + 1)
+  end
+}
